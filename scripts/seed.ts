15e264939b8c7c561b11092d6a97e358e2eb213e
Ni Van Nguyen <ni.vannguyen@infodation.vn>
2025-01-01 13:58:58 +0700

    Seed Script

---
+++ scripts/seed.ts
@@ -0,0 +1,54 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "@/db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+// @ts-ignore
+
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding Database ");
+
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    
+    await db.insert(schema.courses).values([
+      {
+        id: 1,
+        title: "Spanish",
+        imageSrc: "/es.svg",
+      },
+      {
+        id: 2,
+        title: "French",
+        imageSrc: "/fr.svg",
+      },
+      {
+        id: 3,
+        title: "Italian",
+        imageSrc: "/it.svg",
+      },
+      {
+        id: 4,
+        title: "Croation",
+        imageSrc: "/hr.svg",
+      },
+      {
+        id: 5,
+        title: "Japanese",
+        imageSrc: "/jp.svg",
+      },
+    ]);
+
+    console.log("Seeding Finished");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to seed the Database.");
+  }
+};
+
+main();
